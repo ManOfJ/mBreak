cfa75774e214ffcaa6ea8f635b1414532a731312
ManOfJ <manofj@outlook.com>
2016-09-25 21:11:32 +0900

    コードの再構築

--- src/main/scala/com/manofj/minecraft/moj_mbreak/mBreak.scala
+++ src/main/scala/com/manofj/minecraft/moj_mbreak/mBreak.scala
@@ -8,45 +8,45 @@ import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.Mod.EventHandler
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
 
-import com.manofj.minecraft.moj_commons.logging.LoggerLikeMod
-import com.manofj.minecraft.moj_commons.util.MinecraftMod
+import com.manofj.commons.minecraftforge.base.MinecraftForgeMod
+import com.manofj.commons.minecraftforge.i18n.I18nSupportMod
+import com.manofj.commons.minecraftforge.logging.LoggerLikeMod
 
 
 @Mod( modid       = mBreak.modId,
       name        = mBreak.modName,
       version     = mBreak.modVersion,
-      guiFactory  = mBreak.guiFactory,
-      modLanguage = "scala" )
+      modLanguage = mBreak.modLanguage,
+      updateJSON  = mBreak.updateJSON,
+      guiFactory  = mBreak.guiFactory )
 object mBreak
-  extends MinecraftMod
+  extends MinecraftForgeMod
+  with    I18nSupportMod
   with    LoggerLikeMod
 {
-
-  final val modId      = "@modid@"
+  final val modId      = "moj_mbreak"
   final val modName    = "mBreak"
   final val modVersion = "@version@"
+  final val updateJSON = "@updateJson@"
   final val guiFactory = "com.manofj.minecraft.moj_mbreak.mBreakGuiFactory"
 
 
-  // 初期化ブロック
   {
-    // akka のパッケージをトランスフォーム対象外に
     Launch.classLoader.addTransformerExclusion( "akka." )
   }
 
 
   @EventHandler
   def preInit( evt: FMLPreInitializationEvent ): Unit = {
-    import com.manofj.minecraft.moj_commons.config.javaFile2ForgeConfig
+    mBreakConfig.capture( evt.getSuggestedConfigurationFile )
 
-    mBreakConfigHandler.captureConfig( evt.getSuggestedConfigurationFile )
-
+    MinecraftForge.EVENT_BUS.register( mBreakConfig )
     MinecraftForge.EVENT_BUS.register( mBreakEventHandler )
-    MinecraftForge.EVENT_BUS.register( mBreakConfigHandler )
 
-    ClientRegistry.registerKeyBinding( mBreakSettings.miningSpeedMultToggleKey )
-    ClientRegistry.registerKeyBinding( mBreakSettings.chainDestructionToggleKey )
-    ClientRegistry.registerKeyBinding( mBreakSettings.torchAutoPlacementToggleKey )
+    ClientRegistry.registerKeyBinding( mBreakConfig.miningSpeedMultToggleKey )
+    ClientRegistry.registerKeyBinding( mBreakConfig.chainDestructionToggleKey )
+    ClientRegistry.registerKeyBinding( mBreakConfig.torchAutoPlacementToggleKey )
+    ClientRegistry.registerKeyBinding( mBreakConfig.torchPlacementSideToggleKey )
 
   }
 
--- src/main/scala/com/manofj/minecraft/moj_mbreak/mBreakEventHandler.scala
+++ src/main/scala/com/manofj/minecraft/moj_mbreak/mBreakEventHandler.scala
@@ -1,5 +1,7 @@
 package com.manofj.minecraft.moj_mbreak
 
+import java.util.UUID
+
 import akka.actor.Actor
 import akka.actor.ActorRef
 import akka.actor.ActorSystem
@@ -10,14 +12,9 @@ import com.google.common.collect.Maps
 import net.minecraft.block.state.IBlockState
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.entity.player.EntityPlayerMP
-import net.minecraft.init.Blocks.TORCH
 import net.minecraft.item.ItemStack
-import net.minecraft.util.EnumActionResult.SUCCESS
-import net.minecraft.util.EnumFacing.EAST
-import net.minecraft.util.EnumFacing.NORTH
-import net.minecraft.util.EnumFacing.SOUTH
-import net.minecraft.util.EnumFacing.WEST
-import net.minecraft.util.EnumHand.MAIN_HAND
+import net.minecraft.util.EnumActionResult
+import net.minecraft.util.EnumHand
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.text.Style
 import net.minecraft.util.text.TextComponentString
@@ -31,211 +28,220 @@ import net.minecraftforge.fml.client.FMLClientHandler
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent
 
+import com.manofj.commons.scala.util.conversions.Any$
+import com.manofj.commons.scala.util.conversions.Boolean$
+
 
 object mBreakEventHandler {
-  import math.ceil
-  import math.floor
+  import net.minecraft.init.Blocks.TORCH
+  import net.minecraft.util.EnumFacing.EAST
+  import net.minecraft.util.EnumFacing.NORTH
+  import net.minecraft.util.EnumFacing.SOUTH
+  import net.minecraft.util.EnumFacing.WEST
 
-  import net.minecraft.client.resources.I18n.{ format => l10n }
-
-  import com.manofj.minecraft.moj_mbreak.mBreakSettings.{ Local => localSettings }
-  import com.manofj.minecraft.moj_mbreak.{ mBreakSettings => globalSettings }
+  import mBreak.format
+  import mBreakConfig._
 
 
   // アクター: たいまつ設置処理を行うメッセージ
-  private[ this ] case object TorchPlacement
+  private[ this ] case object Placement
 
   // アクター: 自らを破棄するメッセージ
   private[ this ] case object Kill
 
 
+  private[ this ] val actorSystem = ActorSystem( "system" )
+
+  private[ this ] val torchItem = new ItemStack( TORCH )
+
+
   // 連鎖破壊により壊されたブロックの座標リスト
   private[ this ] val positions = Lists.newArrayList[ BlockPos ]
 
-  // アクターシステム
-  private[ this ] val system = ActorSystem( "system" )
-
   // プレイヤーに紐づけられたアクティブなたいまつ設置アクター
-  private[ this ] val activeActor = Maps.newHashMap[ EntityPlayer, ActorRef ]
-
-  // たいまつのアイテムスタック
-  private[ this ] val torchItemStack = new ItemStack( TORCH )
+  private[ this ] val actors = Maps.newHashMap[ UUID, ActorRef ]
 
 
+  // キー入力による各機能のオン/オフを行う
   @SubscribeEvent
   def keyInput( event: KeyInputEvent ): Unit = {
-    def stateText( enabled: Boolean ): String =
-      if ( enabled ) l10n( "moj_mbreak.state.enabled" ) else l10n( "moj_mbreak.state.disabled" )
-
-    def printChatMessage( message: String ): Unit =
-      FMLClientHandler.instance.getClient.ingameGUI.getChatGUI.printChatMessage {
-        new TextComponentString( "" ).appendSibling( new TextComponentString( "[mBreak]: " ).setStyle( new Style().setBold( true ) ) ).appendText( message )
-      }
-
-    if ( globalSettings.miningSpeedMultToggleKey.isKeyDown ) {
-      localSettings.miningSpeedMult = !localSettings.miningSpeedMult
-      printChatMessage( l10n( "moj_mbreak.chat.mining_speedmult", stateText( localSettings.miningSpeedMult ) ) )
+    def printChatMsg( m: String ) = {
+      val chatGui = FMLClientHandler.instance.getClient.ingameGUI.getChatGUI
+      chatGui.printChatMessage( chatMsg( m ) )
     }
-    if ( globalSettings.chainDestructionToggleKey.isKeyDown ) {
-      localSettings.chainDestruction = !localSettings.chainDestruction
-      printChatMessage( l10n( "moj_mbreak.chat.chain_destruction", stateText( localSettings.chainDestruction ) ) )
+    def chatMsg( k: String ) = {
+      val prefix = new TextComponentString( "[mBreak]: " ).setStyle( new Style().setBold( true ) )
+      new TextComponentString( "" ).appendSibling( prefix ).appendText( k )
     }
-    if ( globalSettings.torchAutoPlacementToggleKey.isKeyDown ) {
-      localSettings.torchAutoPlacement = !localSettings.torchAutoPlacement
-      printChatMessage( l10n( "moj_mbreak.chat.torch_auto_placement",
-                        stateText( localSettings.torchAutoPlacement ) ) )
+    def state( f: Boolean ) = format( f ? "state.enabled" ! "state.disabled" )
+
+
+    if ( miningSpeedMultToggleKey.isKeyDown )
+    {
+      miningSpeedmultEnable = !miningSpeedmultEnable
+      printChatMsg( format( "chat.mining_speedmult", state( miningSpeedmultEnable ) ) )
+    }
+
+    if ( chainDestructionToggleKey.isKeyDown )
+    {
+      chainDestruction = !chainDestruction
+      printChatMsg( format( "chat.chain_destruction", state( chainDestruction ) ) )
+    }
+
+    if ( torchAutoPlacementToggleKey.isKeyDown )
+    {
+      torchAutoPlacement = !torchAutoPlacement
+      printChatMsg( format( "chat.torch_auto_placement", state( torchAutoPlacement ) ) )
+    }
+
+    if ( torchPlacementSideToggleKey.isKeyDown )
+    {
+      val toggledSide = ( torchPlacementSide == "left" ) ? "right" ! "left"
+      torchPlacementSide = toggledSide
+      printChatMsg( format( "chat.torch_placement_side", format( s"state.$toggledSide" ) ) )
     }
   }
 
+  // ブロック破壊速度を変更する
   @SubscribeEvent
-  def breakSpeedEvent( event: BreakSpeed ): Unit = {
-    val player = event.getEntityPlayer
-    val canToolHarvestBlock = ForgeHooks.canToolHarvestBlock( player.worldObj, event.getPos, player.getHeldItemMainhand )
+  def breakSpeedEvent( event: BreakSpeed ): Unit =
+    if ( miningSpeedmultEnable && miningSpeedmult > 0 )
+    {
+      val player = event.getEntityPlayer
+      if ( ForgeHooks.canToolHarvestBlock( player.worldObj, event.getPos, player.getHeldItemMainhand ) )
+        event.setNewSpeed( ( event.getOriginalSpeed * miningSpeedmult ).toFloat )
+    }
 
-    if ( localSettings.miningSpeedMult && canToolHarvestBlock )
-      event.setNewSpeed( ( event.getOriginalSpeed * globalSettings.miningSpeedMult ).toFloat )
-
-  }
-
-
+  // 松明の自動設置やブロックの連鎖破壊を行う
   @SubscribeEvent
-  def blockBreak( evt: BreakEvent ): Unit = {
-    // プレイヤーの目線の高さをブロック座標で取得する関数
-    def getEyeHeightY( plyr: EntityPlayer ): Int =
-      floor( plyr.posY + ceil( plyr.getEyeHeight ) - 0.5F ).toInt
+  def blockBreak( event: BreakEvent ): Unit = {
+    import math.ceil
+    import math.floor
+    import scala.concurrent.duration._
 
-    // ブロックを即座に破壊する関数
-    def breakBelowBlock( wld: World, pos: BlockPos, plyr: EntityPlayerMP ): Unit = {
-      if (    !wld.isAirBlock( pos )
-           && ForgeHooks.canToolHarvestBlock( wld, pos, plyr.getHeldItemMainhand ) )
+    import actorSystem.dispatcher
+
+
+    def blockDestroy( w: World, b: BlockPos, p: EntityPlayerMP ): Unit = {
+      if ( !w.isAirBlock( b ) &&
+           ForgeHooks.canToolHarvestBlock( w, b, p.getHeldItemMainhand ) )
       {
-        positions.add( pos )
-        wld.sendBlockBreakProgress( plyr.getEntityId, pos, -1 )
-        plyr.interactionManager.tryHarvestBlock( pos )
+        positions.add( b )
+        w.sendBlockBreakProgress( p.getEntityId, b, -1 )
+        p.interactionManager.tryHarvestBlock( b )
       }
     }
 
-    // たいまつ自動設置処理を実行できる条件がそろっているか評価する関数
-    def tAutoPlacementCheckCondition( plyr:  EntityPlayerMP,
-                                      pos:   BlockPos,
-                                      state: IBlockState ): Boolean =
-      localSettings.torchAutoPlacement  &&
-      globalSettings.torchAutoPlacement &&
-      !positions.contains( pos )        &&
-      state.getBlock != TORCH           &&
-      ForgeHooks.canToolHarvestBlock( plyr.worldObj, pos, plyr.getHeldItemMainhand )
+    def eyeHeightY( p: EntityPlayer ) = floor( p.posY + ceil( p.getEyeHeight ) - 0.5F ).toInt
+
+    def canAutoPlacement( p: EntityPlayerMP, b: BlockPos, s: IBlockState ) =
+      torchAutoPlacement &&
+      b.getY == eyeHeightY( p ) &&
+      s.getBlock != TORCH &&
+      ForgeHooks.canToolHarvestBlock( p.worldObj, b, p.getHeldItemMainhand )
+
+    def isDarker( w: World, b: BlockPos ) = w.getLightFromNeighbors( b ) <= 7
 
 
-    // 座標の明るさレベルが 7 以下であるか評価する関数
-    def isDarker( wld: World, pos: BlockPos ): Boolean =
-      wld.getLightFromNeighbors( pos ) <= 7
-
-
-    evt.getPlayer match {
-      case player: EntityPlayerMP => // サーバー側の処理
-        import evt._
-        if ( tAutoPlacementCheckCondition( player, getPos, getState ) ) // たいまつ自動設置処理
+    val world = event.getWorld
+    val pos   = event.getPos
+    ( !world.isRemote, event.getPlayer ) match {
+      case ( true, player: EntityPlayerMP ) =>
+        // 松明の自動設置処理
+        if ( canAutoPlacement( player, pos, event.getState ) )
         {
-          val sideOpt = player.getHorizontalFacing match {
-            case NORTH | SOUTH  => Option( WEST )
-            case WEST  | EAST   => Option( NORTH )
-            case _              => None
+          val placeLeft = torchPlacementSide == "left"
+
+          var side = player.getHorizontalFacing match {
+            case NORTH => placeLeft ? EAST ! WEST
+            case SOUTH => placeLeft ? WEST ! EAST
+            case WEST  => placeLeft ? NORTH ! SOUTH
+            case EAST  => placeLeft ? SOUTH ! NORTH
+          }
+          var hit  = pos.offset( side.getOpposite )
+
+          // 指定されたサイドに松明を設置できそうになければ
+          // 逆サイドへの変更を試みる
+          if ( !world.isSideSolid( hit, side ) )
+          {
+            val oppositePos = pos.offset( side )
+            val oppositeSide = side.getOpposite
+            if ( world.isSideSolid( oppositePos, oppositeSide ) )
+            {
+              side = oppositeSide
+              hit  = oppositePos
+            }
           }
 
-          sideOpt match {
-            case Some( side ) =>
-              import scala.concurrent.duration._
+          val uuid = player.getUniqueID
+          val actor = actorSystem.actorOf { Props { new Actor {
+            private[ this ] val counter = Range( 0, 5 ).iterator
+            private[ this ] def placement = torchItem.onItemUse(
+              player,
+              world,
+              pos,
+              EnumHand.MAIN_HAND,
+              side,
+              hit.getX,
+              hit.getY,
+              hit.getZ
+            )
 
-              import system.dispatcher
+            override def receive: Receive = {
+              case Kill =>
+                if ( actors.containsValue( self ) ) actors.remove( uuid )
+                counter.contains( 0 ) ? ( self ! Placement ) ! actorSystem.stop( self )
 
-              val hitPos = getPos.add( 0.5D, 0.5D, 0.5D )
-              // たいまつの設置処理を行うアクター
-              val tPlacementActor = system.actorOf {
-                Props { new Actor {
-                  // 実行回数カウンタ
-                  private[ this ] val counter = Stream.from( 1 ).iterator
-
-                  // たいまつの設置を試みる関数
-                  private[ this ] def torchPlacement() =
-                    torchItemStack.onItemUse(
-                      player,
-                      getWorld,
-                      getPos,
-                      MAIN_HAND,
-                      side,
-                      hitPos.getX,
-                      hitPos.getY,
-                      hitPos.getZ
-                    ) == SUCCESS
-
-                  // アクター処理内容
-                  override def receive: Receive = {
-                    case Kill => counter.next match {
-                      case 1 => // たいまつ設置処理を行っていなければ一度だけ､即座に実行する
-                        counter.dropWhile( _ < 5 )
-                        self ! TorchPlacement
-                      case _ => system.stop( self )
-                    }
-                    case TorchPlacement =>
-                      // プレイヤーがたいまつを所持していることが前提条件
-                      if ( player.inventory.hasItemStack( torchItemStack ) ) {
-                        // 指定座標が空気ブロックで､なおかつ明るさレベルが 7 以下の場合
-                        // プレイヤーのインベントリからたいまつを使用する
-                        if ( getWorld.isAirBlock( getPos ) && isDarker( getWorld, getPos ) ) {
-                          torchItemStack.stackSize = 64
-                          if ( torchPlacement() ) {
-                            val slot = player.inventory.getSlotFor( torchItemStack )
-                            player.inventory.decrStackSize( slot, 1 )
-                            if ( !player.inventory.hasItemStack( torchItemStack ) )
-                              player.addChatMessage { new TextComponentTranslation(
-                                l10n( "moj_mbreak.chat.torch_runout" )
-                              ) }
-
-                            // 処理が完了したためカウンタを進める
-                            counter.dropWhile( _ < 5 )
-                          }
-                        }
+              case Placement =>
+                val inventory = player.inventory
+                if ( !inventory.hasItemStack( torchItem ) ) self ! Kill
+                else if ( !counter.hasNext ) self ! Kill
+                else
+                {
+                  counter.next()
+                  if ( world.isAirBlock( pos ) && isDarker( world, pos ) )
+                  {
+                    torchItem.stackSize = 64
+                    if ( placement == EnumActionResult.SUCCESS )
+                    {
+                      inventory.decrStackSize( inventory.getSlotFor( torchItem ), 1 )
+                      if ( !inventory.hasItemStack( torchItem ) )
+                      {
+                        val msg = format( "chat.torch_runout" )
+                        player.addChatMessage( new TextComponentTranslation( msg ) )
                       }
-                      // 3 回実行したら自らを破棄する
-                      if ( counter.next >= 3 ) self ! Kill
+                      self ! Kill
+                    }
                   }
-                }}
-              }
+                }
+            }
+          } } }
 
-              // アクターのスケジューラにたいまつ設置アクターを設定する
-              system.scheduler.schedule( 250.millisecond, 250.millisecond, tPlacementActor, TorchPlacement )
+          actorSystem.scheduler.schedule( 200.millisecond, 200.millisecond, actor, Placement )
 
-              // たいまつ設置アクターをプレイヤーに紐づける
-              Option( activeActor.put( player, tPlacementActor ) ) match {
-                // まだ稼働している古いたいまつ設置アクターを破棄する
-                case Some( actor ) => actor ! Kill
-                case None => // 何もしない
-              }
-            case None         =>
-              mBreak.warn(
-                "Illegal EnumFacing from EntityPlayerMP#getHorizontalFacing" )
-          }
-
+          actors.get( uuid ).?.foreach( _ ! Kill )
+          actors.put( uuid, actor )
         }
-        if ( localSettings.chainDestruction && globalSettings.chainDestruction ) // 連鎖破壊処理
+
+        // 連鎖破壊処理
+        if ( chainDestruction )
         {
-          if ( positions contains getPos ) // 連鎖破壊により壊されたブロックの場合
+          if ( positions.contains( pos ) )
           {
-            // ブロックのY座標がプレイヤーのY座標より高ければ､さらに下のブロックを破壊する
-            if ( getPos.getY > player.posY )
-              breakBelowBlock( getWorld, getPos.down, player )
+            if ( pos.getY > player.posY )
+            {
+              blockDestroy( world, pos.down, player )
+            }
 
-            positions remove getPos
+            positions.remove( pos )
           }
-          else // 正規の手段で壊されたブロックの場合
+          else if ( pos.getY == eyeHeightY( player ) )
           {
-            // ブロックの高さがプレイヤーの目線の高さと同じなら､下のブロックを破壊する
-            if ( getPos.getY == getEyeHeightY( player ) )
-              breakBelowBlock( getWorld, getPos.down, player )
+              blockDestroy( world, pos.down, player )
           }
         }
-      case _ =>
-        // クライアント側では何もしない
+
+      case _ => // 何もしない
     }
   }
 
